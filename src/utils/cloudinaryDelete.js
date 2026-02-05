39d0ddd89961d6a2e269d0858cd6dad1329df0c8
Raja Kumar <37rajakumar@gmail.com>
2026-02-05 12:28:14 +0530

    Update cloudinaryDelete.js

--- src/utils/cloudinaryDelete.js
+++ src/utils/cloudinaryDelete.js
@@ -1,17 +1,12 @@
 import cloudinary from "./cloudinary.js";
 
-/*
-  Extract public_id from Cloudinary URL and delete image
-*/
 export const deleteFromCloudinary = async (imageUrl) => {
   if (!imageUrl) return;
 
   try {
-    // example URL:
-    // https://res.cloudinary.com/xxx/image/upload/v123/rkblog_posts/abc.png
 
     const parts = imageUrl.split("/");
-    const fileName = parts.pop().split(".")[0]; // abc
+    const fileName = parts.pop().split(".")[0]; 
     const folder = parts.slice(parts.indexOf("upload") + 2).join("/");
 
     const publicId = `${folder}/${fileName}`;
