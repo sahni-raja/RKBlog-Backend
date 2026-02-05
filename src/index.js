99b83c2f98f160b3648bbe97fadb6d18334e92d5
Raja Kumar <37rajakumar@gmail.com>
2026-02-05 12:11:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-//import dotenv from "dotenv";
-//dotenv.config({ path: "./.env" });
-
-// import "dotenv/config"; 
-
-// import app from "./app.js";
-// import connectDB from "./db/index.js";
-
-// const PORT = process.env.PORT || 5000;
-
-// connectDB().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`🚀 Server running on port ${PORT}`);
-//   });
-// });
-
 import "dotenv/config";
 import http from "http";
 
