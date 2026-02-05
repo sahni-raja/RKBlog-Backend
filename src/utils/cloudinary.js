85b6ea2a638dac4404bbe27bba2b3ea454e3e860
Raja Kumar <37rajakumar@gmail.com>
2026-02-05 12:27:14 +0530

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,4 @@
 import { v2 as cloudinary } from "cloudinary";
-//console.log("CLOUDINARY NAME:", process.env.CLOUDINARY_CLOUD_NAME);
-//console.log("CLOUDINARY KEY:", process.env.CLOUDINARY_API_KEY);
-//console.log("CLOUDINARY SECRET:", process.env.CLOUDINARY_API_SECRET);
-
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
