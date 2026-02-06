28890c2ac912ded3d599b9af89215cbf70242914
Raja Kumar <37rajakumar@gmail.com>
2026-02-06 20:31:32 +0530

    Add new localhost URL to allowed origins

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
   "http://localhost:3000",
+  "http://localhost:3001",
   "http://localhost:5176",
   "https://rkblog-frontend.vercel.app", // when you deploy frontend
 ];
