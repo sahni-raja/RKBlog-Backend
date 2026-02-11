e8ddc0aabb93f037b28ea79a50d3af49bfdb7caf
Raja Kumar <37rajakumar@gmail.com>
2026-02-12 01:13:17 +0530

    Enable user profile routes
    Uncommented user routes for profile access and updates.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,3 @@
-// import express from "express";
-// import { 
-//   getPublicUserProfile, 
-//   updateUserProfile 
-// } from "../controllers/user.controller.js";
-// import { protect } from "../middlewares/auth.middleware.js"; // Ensure this path is correct
-
-// const router = express.Router();
-
-// // Public: View anyone's profile
-// router.get("/:userId", getPublicUserProfile);
-
-// // Private: Update my own profile
-// router.put("/update", protect, updateUserProfile);
-
-// export default router;
 import express from "express";
 import { 
   getPublicUserProfile, 
