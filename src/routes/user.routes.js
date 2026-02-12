0d600429645981ce2302829ea33f6b5fabfecebf
Raja Kumar <37rajakumar@gmail.com>
2026-02-12 16:37:36 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,15 +4,12 @@ import {
   updateUserProfile 
 } from "../controllers/user.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js"; // <--- Import Multer
+import { upload } from "../middlewares/multer.middleware.js"; 
 
 const router = express.Router();
 
-// Public: View anyone's profile
 router.get("/:userId", getPublicUserProfile);
 
-// Private: Update profile (Now supports file upload)
-// We use "avatar" as the field name for the file
 router.put("/update", protect, upload.single("avatar"), updateUserProfile);
 
 export default router;
